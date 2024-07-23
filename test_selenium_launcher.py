245258866df3aa312b870ddab944d74577454985
skoli35r <89765667+skoli35r@users.noreply.github.com>
2024-07-23 23:18:09 +0530

    Update test_selenium_launcher.py

--- test_selenium_launcher.py
+++ test_selenium_launcher.py
@@ -1,6 +1,6 @@
 import pytest
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+#from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 
@@ -8,7 +8,7 @@ def test_a():
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
     cdm = ChromeDriverManager().install()
-    driver = webdriver.Chrome(cdm, options=options)
+    driver = webdriver.Chrome("chromedriver", options=options)
 
     driver.get("https://discuss.python.org/t/understanding-site-packages-directories/12959")
     time.sleep(5)
