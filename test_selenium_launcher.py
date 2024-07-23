854faf961dc20ebf8bbb271cbeda01ae4a1534f3
skoli35r <skoli35@rediffmail.com>
2024-07-23 22:52:25 +0530

    ok

---
+++ test_selenium_launcher.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+
+def test_a():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    cdm = ChromeDriverManager().install()
+    driver = webdriver.Chrome(cdm, options=options)
+
+    driver.get("https://discuss.python.org/t/understanding-site-packages-directories/12959")
+    time.sleep(5)
+    print(driver.title)
